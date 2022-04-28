896cb4644ca9fab82077630f8e3ed545e2f9406e
jesperfribergspang <jesperfriberg94@hotmail.com>
2022-04-28 17:14:59 +0200

    Initial commit

---
+++ src/main/java/com/example/catprojectrabbitconsumer/CatProjectRabbitConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.catprojectrabbitconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatProjectRabbitConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatProjectRabbitConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/catprojectrabbitconsumer/CatProjectRabbitConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.catprojectrabbitconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatProjectRabbitConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
